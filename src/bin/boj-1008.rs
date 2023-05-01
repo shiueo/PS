7b90aa1c481cf281d7c8c6740b2a9787f8a91300
shiüo <shiueo.csh@gmail.com>
2023-05-01 12:12:10 +0900

    d

--- src/bin/boj-1008.rs
+++ src/bin/boj-1008.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    let tokens:Vec<i32> = input.as_mut_str().split_ascii_whitespace().map(|input| input.parse().unwrap()).collect();
+    let tokens:Vec<f64> = input.as_mut_str().split_ascii_whitespace().map(|input| input.parse().unwrap()).collect();
 
-    println!("{}", tokens[0] / tokens[1]);
+    println!("{:.20}", tokens[0] / tokens[1]);
 }
