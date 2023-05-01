0164ffd3173be80a7636f70058fe08a65541f1cb
shiüo <shiueo.csh@gmail.com>
2023-05-01 13:03:03 +0900

    f

--- web/components/ClientOnly.tsx
+++
@@ -1,7 +0,0 @@
-import { isBrowser } from '~/lib/is-browser';
-
-const ClientOnly: React.FCC = ({ children }) => {
-  return isBrowser() ? <>{children}</> : null;
-};
-
-export default ClientOnly;
--- web/components/Footer.tsx
+++
@@ -1,5 +0,0 @@
-const Footer: React.FCC = () => {
-  return <div>Makerkit - {new Date().getFullYear()}</div>;
-};
-
-export default Footer;
