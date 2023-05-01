e7d0986ed198c15e17677540e070f6a2c0829259
shiüo <shiueo.csh@gmail.com>
2023-05-01 12:06:01 +0900

    rust

---
+++ src/bin/boj-1001.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let tokens:Vec<i32> = input.as_mut_str().split_ascii_whitespace().map(|input| input.parse().unwrap()).collect();
+
+    println!("{}", tokens[0] - tokens[1]);
+}
