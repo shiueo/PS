ead42864b975aeee221f2781970b1cf00b80e9c2
shiüo <shiueo.csh@gmail.com>
2023-05-04 13:13:05 +0900

    s

--- src/bin/boj-1788.rs
+++ src/bin/boj-1788.rs
@@ -4,40 +4,34 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    let mut n:i32 = input.trim().parse().unwrap();
+    let mut n: i32 = input.trim().parse().unwrap();
 
-    let mut fibonacci:Vec<i32> = vec![0,1];
+    let mut fibonacci: Vec<i32> = vec![0, 1];
     if n > 0 {
         for i in 1..n as usize {
-            fibonacci.push((fibonacci[i] + fibonacci[i-1])% 1000000000);
+            fibonacci.push((fibonacci[i] + fibonacci[i - 1]) % 1000000000);
         }
-        n = fibonacci[fibonacci.len()-1];
-    }
-        else if n == 0 {
-            fibonacci.pop();
-        }
-    else {
+        n = fibonacci[fibonacci.len() - 1];
+    } else if n == 0 {
+        fibonacci.pop();
+    } else {
         n = n.abs();
         for i in 1..n as usize {
-            fibonacci.push((fibonacci[i] + fibonacci[i-1])% 1000000000);
+            fibonacci.push((fibonacci[i] + fibonacci[i - 1]) % 1000000000);
         }
         if n % 2 == 0 {
-            n = -1*fibonacci[fibonacci.len()-1];
-        }
-        else {
-            n = fibonacci[fibonacci.len()-1];
+            n = -1 * fibonacci[fibonacci.len() - 1];
+        } else {
+            n = fibonacci[fibonacci.len() - 1];
         }
     }
 
     if n > 0 {
         println!("1");
-    }
-    else if n == 0 {
+    } else if n == 0 {
         println!("0");
-    }
-    else {
+    } else {
         println!("-1");
     }
     println!("{}", n.abs());
 }
-
--- src/bin/boj-1912.rs
+++ src/bin/boj-1912.rs
@@ -1,5 +1,5 @@
-use std::io;
 use std::cmp;
+use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
@@ -8,10 +8,10 @@ fn main() {
     let arr: Vec<_> = input.skip(1).collect();
 
     let mut v_arr = vec![arr[0]];
-    let mut max_v:i32 = -1000;
+    let mut max_v: i32 = -1000;
 
     for i in 1..arr.len() {
-        let v = arr[i].max(arr[i] + v_arr[i-1]);
+        let v = arr[i].max(arr[i] + v_arr[i - 1]);
         v_arr.push(v);
         max_v = cmp::max(max_v, v)
     }
