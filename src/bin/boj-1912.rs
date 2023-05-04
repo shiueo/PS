33062fb1edd8cbb757e521d6b82152a4c5d469e8
shiüo <shiueo.csh@gmail.com>
2023-05-04 13:06:50 +0900

    d

---
+++ src/bin/boj-1912.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::cmp;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let arr: Vec<_> = input.skip(1).collect();
+
+    let mut v_arr = vec![arr[0]];
+    let mut max_v:i32 = -1000;
+
+    for i in 1..arr.len() {
+        let v = cmp::max(arr[i], arr[i]+v_arr[i-1]);
+        v_arr.push(v);
+        max_v = cmp::max(max_v, v)
+    }
+    println!("{}", v_arr.iter().max().unwrap());
+}
