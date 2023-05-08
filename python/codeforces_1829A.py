106e6bc84493d71ab4c1d27082ced46aff213dcf
shiüo <shiueo.csh@gmail.com>
2023-05-08 10:21:17 +0900

    d

--- python/codeforces_1829A.py
+++ python/codeforces_1829A.py
@@ -3,9 +3,9 @@ import sys
 n = int(sys.stdin.readline())
 CODEFORCES = 'codeforces'
 for i in range(n):
-    c=0
+    c = 0
     inn = sys.stdin.readline().strip()
     for k in range(len(inn)):
         if inn[k] != CODEFORCES[k]:
-            c+=1
+            c += 1
     print(c)
