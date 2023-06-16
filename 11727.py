1b8d3fa4d039e50430dcb6902d3734504b31a94f
shiüo <shiueo.csh@gmail.com>
2023-06-16 11:48:53 +0900

    f

---
+++ 11727.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0 for _ in range(n)]
+
+if n >= 3:
+    dp[0] = 1
+    dp[1] = 3
+    for i in range(2, n):
+        dp[i] = dp[i - 2]*2 + dp[i - 1]
+    print(dp[-1] % 10007)
+else:
+    if n == 2:
+        print(3)
+
+    else:
+        print(1)
---
+++ 9461.py
@@ -0,0 +1,15 @@
+import sys
+
+X = [0 for i in range(101)]
+
+X[1] = 1
+X[2] = 1
+X[3] = 1
+
+for i in range(4, 101):
+    X[i] = X[i-3] + X[i-2]
+
+n = int(sys.stdin.readline())
+for i in range(n):
+    u = int(sys.stdin.readline())
+    print(X[u])
