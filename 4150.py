eb33d482fe88dd1883f4da16014afcd6a00efa1b
shiüo <shiueo.csh@gmail.com>
2023-06-18 02:31:46 +0900

    s

---
+++ 4150.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+fibo = [0 for i in range(N)]
+if N >= 2:
+    fibo[0] = 1
+    fibo[1] = 1
+    for i in range(2, N):
+        fibo[i] = fibo[i - 1] + fibo[i - 2]
+    print(fibo[-1])
+else:
+    if N == 1:
+        print('1')
+    else:
+        print('0')
