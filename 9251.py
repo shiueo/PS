5ad64f5318fb5600fb89d18490d13880ab0c5b1d
shiüo <shiueo.csh@gmail.com>
2023-06-18 04:47:14 +0900

    Update 9251.py

--- 9251.py
+++ 9251.py
@@ -9,8 +9,8 @@ dp = [[0 for _ in range(L_A+1)] for _ in range(L_B+1)]
 for i in range(1, L_B+1):
     for j in range(1, L_A+1):
         if B[i-1] == A[j-1]:
-            dp[i][j] = dp[i][j-1]+1
+            dp[i][j] = dp[i-1][j-1]+1
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
 print(dp[-1][-1])
