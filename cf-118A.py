7df7cfe066ef1d710999eaca7e07f48a39ac53f9
shiüo <shiueo.csh@gmail.com>
2023-06-21 03:33:38 +0900

    d

---
+++ cf-118A.py
@@ -0,0 +1,9 @@
+import sys
+
+x = list(sys.stdin.readline().strip().lower())
+
+vowels = ["a", "e", "i", "o", "u", "y"]
+
+
+x = [_ for _ in x if _ not in vowels]
+print('.'+'.'.join(x))
---
+++ cf-281A.py
@@ -0,0 +1,5 @@
+import sys
+
+x = sys.stdin.readline().strip()
+
+print(x[0].upper()+x[1:])
